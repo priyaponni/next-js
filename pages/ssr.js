ae0fd44b3b777eee2ae1657f56c267b854090dec
ppuliyurudaiyan <ppuliyurudaiyan@paypal.com>
2021-09-10 16:17:19 -0700

    ssr, static

---
+++ pages/ssr.js
@@ -0,0 +1,30 @@
+function AboutSSR({name, bpi}) {
+    const bpi1 = Object.keys(bpi).map(x => {
+            return <div key={x}> {bpi[x].rate} {bpi[x].code} </div>
+        });
+    
+    return <div>
+        Name:   {name}
+        <br/>
+        {bpi1}
+    </div>
+  }
+
+  export async function getServerSideProps() {
+
+   // Call an external API endpoint to get posts
+  const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+  const result = await res.json()
+  console.log('**** getServerSideProps');
+  console.log(result);
+
+    return {
+      props: {
+          name: result.chartName,
+          bpi: result.bpi
+      }, // will be passed to the page component as props
+    }
+  }
+
+
+  export default AboutSSR
